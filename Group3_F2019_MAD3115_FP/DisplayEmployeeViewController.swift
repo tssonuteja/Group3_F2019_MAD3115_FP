5b189fbdb32f9a149989d6e02274bdb1669c2b5c
sai teja <saitejasunkari04@gmail.com>
2019-11-15 18:48:00 -0500

    created DisplayEmployeeViewController

---
+++ Group3_F2019_MAD3115_FP/DisplayEmployeeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DisplayEmployeeViewController.swift
+//  Group3_F2019_MAD3115_FP
+//
+//  Created by Sai Teja on 2019-11-15.
+//  Copyright © 2019 Sai Teja. All rights reserved.
+//
+
+import UIKit
+
+class DisplayEmployeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
