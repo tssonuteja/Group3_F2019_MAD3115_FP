bd7c6c5e0d14a62535ffe0c7e25a28b43d9f1164
Jeenapaul <jeenapaul1014@gmail.com>
2019-11-15 20:55:25 -0500

    FullTime class added

--- Group3_F2019_MAD3115_FP/Swift Files/FullTime.swift
+++ Group3_F2019_MAD3115_FP/Swift Files/FullTime.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+class FullTimeEmployee: Employee {
+    var salary: Double?
+    var bonus: Double?
+    
+    
+    func calcEarning() -> Double {
+        return salary!*((100+bonus!)/100)
+    }
+    
+    
+    
+    
+    
+    override func printMyData() {
+        super.printMyData()
+        print("Employee Type is Full Time")
+        print("Employee total Salary is =  \(calcEarning())")
+        
+    }
+    
+    
+}
