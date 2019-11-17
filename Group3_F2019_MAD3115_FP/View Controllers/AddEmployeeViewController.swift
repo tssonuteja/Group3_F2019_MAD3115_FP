c9a7e04627722286a23d4017e15aca1c831b8e00
Jeenapaul <jeenapaul1014@gmail.com>
2019-11-16 23:13:51 -0500

    Working on AddEmployeeViewController

--- Group3_F2019_MAD3115_FP/View Controllers/AddEmployeeViewController.swift
+++ Group3_F2019_MAD3115_FP/View Controllers/AddEmployeeViewController.swift
@@ -9,23 +9,18 @@
 import UIKit
 
 class AddEmployeeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtFullName: UITextField!
+    @IBOutlet weak var txtEmpID: UITextField!
+    @IBOutlet weak var txtVehicleMake: UITextField!
+    @IBOutlet weak var txtVehiclePlate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //test
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
