8f1b0b7c726d440b6d51a7564cdf32b1f86b8081
Jeenapaul <jeenapaul1014@gmail.com>
2019-11-16 23:24:30 -0500

    issued connections textfields

--- Group3_F2019_MAD3115_FP/View Controllers/AddEmployeeViewController.swift
+++ Group3_F2019_MAD3115_FP/View Controllers/AddEmployeeViewController.swift
@@ -15,6 +15,17 @@ class AddEmployeeViewController: UIViewController {
     @IBOutlet weak var txtEmpID: UITextField!
     @IBOutlet weak var txtVehicleMake: UITextField!
     @IBOutlet weak var txtVehiclePlate: UITextField!
+    @IBOutlet weak var txtVehicleColour: UITextField!
+    
+    
+    
+    @IBOutlet weak var txtStipend: UITextField!
+    @IBOutlet weak var txtSchoolName: UITextField!
+    @IBOutlet weak var txtFullTimeCommissionPercentage: UITextField!
+    @IBOutlet weak var txtFullTimeSalary: UITextField!
+    @IBOutlet weak var txtFixedBasedSalary: UITextField!
+    @IBOutlet weak var txtFixedBasedHourlyRate: UITextField!
+    @IBOutlet weak var txtFixedBasedHoursWorked: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
