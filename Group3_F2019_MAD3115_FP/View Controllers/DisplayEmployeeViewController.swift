9b36c1b600caee9c5004f64367645b5c7a3b4c79
sai teja <saitejasunkari04@gmail.com>
2019-11-17 13:48:27 -0500

    connections issued in DispleyEmployeeDetailsViewController

--- Group3_F2019_MAD3115_FP/View Controllers/DisplayEmployeeViewController.swift
+++ Group3_F2019_MAD3115_FP/View Controllers/DisplayEmployeeViewController.swift
@@ -14,23 +14,42 @@ class DisplayEmployeeViewController: UIViewController {
     var fullTimeArray: [FullTimeEmployee] = [FullTimeEmployee]()
     var cbptArray: [CommisionBasedPartTime] = [CommisionBasedPartTime]()
     var fbptArray: [FixedBasePartTime] = [FixedBasePartTime]()
-
+    
+    
+    
+    @IBOutlet weak var txtEmployeeID: UITextField!
+    
+    
+    
+    @IBOutlet weak var lblEmployeeName: UILabel!
+    
+    
+    @IBOutlet weak var lblEmployeeBirthday: UILabel!
+    
+    
+    @IBOutlet weak var lblEmployeeHourlyRate: UILabel!
+    
+    
+    
+    @IBOutlet weak var lblEmployeeSalary: UILabel!
+    
+    
+    @IBOutlet weak var lblEmployeeVehicleType: UILabel!
+    
+    
+    @IBOutlet weak var lblEmployeeVehicleMake: UILabel!
+    
+    
+    
+    @IBOutlet weak var lblEmployeeVehiclePlate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        //test
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
