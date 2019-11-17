5dfff8b70f2545ae4aade805c88b2455df169440
sai teja <saitejasunkari04@gmail.com>
2019-11-17 15:58:11 -0500

    connections issued in UpdateEmployeeDetailsViewController

---
+++ Group3_F2019_MAD3115_FP/View Controllers/UpdateEmployeeProfileViewController.swift
@@ -0,0 +1,53 @@
+//
+//  EditEmployeeProfileViewController.swift
+//  Group3_F2019_MAD3115_FP
+//
+//  Created by Sai Teja on 2019-11-15.
+//  Copyright © 2019 Sai Teja. All rights reserved.
+//
+
+import UIKit
+
+class UpdateEmployeeProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtUEmployeeID: UITextField!
+    
+    
+    @IBOutlet weak var txtUName: UITextField!
+    
+    
+    @IBOutlet weak var txtUEmailID: UITextField!
+    
+    
+    @IBOutlet weak var txtUBirthDate: UITextField!
+    
+    
+    @IBOutlet weak var txtLatitude: UITextField!
+    
+    
+    
+    @IBOutlet weak var txtLongitude: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      //test
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    @IBAction func btnSave(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func btnCancel(_ sender: Any) {
+    }
+    
+
+    
+
+}
